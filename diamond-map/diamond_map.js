a9040f551086dc3f200cc000ab28ba52b3cf8251
14TEvett <14TEvett@ka.vale-academy.org>
2024-07-11 16:44:03 +0100

    Nearest Beamline

--- diamond-map/diamond_map.js
+++ diamond-map/diamond_map.js
@@ -3,6 +3,7 @@ var map = L.map('map').setView([51.574349, -1.310892], 17);
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+map.attributionControl.addAttribution('<a href="https://www.diamond.ac.uk">Diamond Light Source</a>');
 
 const lowerBoundary = [51.57168183170403, -1.3173294067382815];
 const upperBoundary = [51.57701619673675, -1.304454803466797];
@@ -13,7 +14,6 @@ L.imageOverlay(imageUrl, imageBounds,{zindex: 1}).addTo(map);
 
 var imageUrl = "BaseOver.png";
 L.imageOverlay(imageUrl, imageBounds,{zindex: 2}).addTo(map);
-
 var greenIcon = new L.Icon({
     iconUrl: 'diamond_light_source_image.png',
     iconSize: [204.4, 60],
@@ -30,14 +30,18 @@ var marker = L.marker([51.574349, -1.310892], {icon: greenIcon}).addTo(map);
 //     alert(ev.latlng); // ev is an event object (MouseEvent in this case)
 // });
 map.locate({watch: true})
-var circle = L.circle([51.574349, -1.310892], {radius: 10}).addTo(map);
-var circle2 = L.circle([51.574349, -1.310892], {radius: 4, color: '#ff0000'}).addTo(map);
-
+var circle = L.circle([51.574349, -1.310892], {radius: 10, color: '#fbd40c'}).addTo(map);
+var circle2 = L.circle([51.574349, -1.310892], {radius: 4, color: '#242d64'}).addTo(map);
+// var circle3 = L.circle([51.574349, -1.310892], {radius: 4, color: '#242d64'}).addTo(map); 
+var loc = [51.574349, -1.310892];
+var distancesArray = [];
+var markersArray = [];
 map.on('locationfound', function(ev) {
-    console.log(ev.latlng);
+    loc = ev.latlng;
     circle.setLatLng(ev.latlng);
     circle.setRadius(ev.accuracy);
     circle2.setLatLng(ev.latlng);
+    
 
 
 })
@@ -45,8 +49,6 @@ fetch("beamlines_data.json").then((result) => result.json()).then((groups) => {
     var overlays = {};
     for (var group of groups) {
         let lg = L.layerGroup()
-        console.log(group)
-        console.log(group.marker)
         var icon = new L.icon({
             iconUrl: group.marker,
             shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -61,11 +63,41 @@ fetch("beamlines_data.json").then((result) => result.json()).then((groups) => {
             var position = beamline.position
             var link = `<a href=${beamline.url}>${beamline.url}</a>`
             var marker = L.marker(position, {icon: icon}).addTo(lg);
+            markersArray.push(marker)
             marker.bindPopup(`<b>${beamline.name}</b><br>${group.name}<br>${beamline.description}<br>${link}`)
 
         }
+
         overlays[group.name] = lg
         lg.addTo(map)
     }
     L.control.layers(null,overlays).addTo(map)
 })
+
+var controlButton = L.control({position: "topright"});
+controlButton.onAdd = function () {
+    var div = L.DomUtil.create('div');
+    div.innerHTML = '<button>Find nearest beamline</button>';
+    div.firstChild.addEventListener("click", function(ev) {
+        L.DomEvent.stopPropagation(ev);
+        console.log("Hello world");
+        var checkingDistance = Number.MAX_SAFE_INTEGER
+        console.log(distancesArray)
+        for (var marker of markersArray) {
+            var markerPosition = marker.getLatLng();
+            var distance = markerPosition.distanceTo(loc)
+            if(distance<checkingDistance) {
+                checkingDistance = distance
+                var closestMarker = marker
+            } 
+        }
+        console.log(checkingDistance)
+        closestMarker.openPopup();
+        closestMarker.setPopupContent('<b>Nearest beamline</b>')
+
+    }); 
+    return div
+
+
+};
+controlButton.addTo(map);
